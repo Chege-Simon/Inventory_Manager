bd2bc53555c466e352ca83480dfb661dd1a2414b
Simon-Chege <petekim211@gmail.com>
2023-09-08 17:05:08 +0300

    sh file composer --no-scripts

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,8 @@ echo 'Running composer'
 composer global require hirak/prestissimo
 composer dump-autoload
 # composer install --no-dev --working-dir=/var/www/html
-composer install --no-dev
+# composer install --no-dev
+composer install --no-scripts
 
 echo 'Caching config...'
 php artisan config:cache
