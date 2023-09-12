0113c5c670cea4db55317cf1ae88e08ced3c0972
Simon-Chege <petekim211@gmail.com>
2023-09-12 12:00:51 +0300

    updating script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 echo 'Running composer'
 composer global require hirak/prestissimo
+
+echo 'composer dump-autoload'
 composer dump-autoload
 composer self-update
 composer clear-cache
+
+echo 'composer update'
+composer update
+
+echo 'composer install'
 composer install --no-dev --working-dir=/var/www/html
 # composer install --no-dev
 # composer install --no-scripts
